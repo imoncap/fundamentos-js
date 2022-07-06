a47df8c3198edc7c2b96e57b706c2687004ca1c4
imoncap <imoncap.dev@gmail.com>
2022-07-06 10:42:35 -0300

    exemplos da aula 1

---
+++ 1-aulaTipoNumber.js
@@ -0,0 +1,26 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuante = .5;
+
+const novaOperacao = segundoNumero * numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN -> Not A Number (não é um número)
+
+const Alura = "Alura";
+console.log(Alura * primeiroNumero)
+
+console.log("Encerrada a aula 1. ")
---
+++ 2-aulaTipoString.js
@@ -0,0 +1,13 @@
+const texto1 = 'Olá, mundo!';
+const texto2 = "Olá, mundo!";
+const senha = 'senhaSuperSegura456!'
+const stringDeNumeros = '34567';
+const citacao = 'Meu nome é: ';
+const meuNome = 'Igor';
+
+// Concatenação (+)
+console.log('-')
+console.log(citacao + meuNome)
+console.log('-')
+
+// Template string ou template literal
